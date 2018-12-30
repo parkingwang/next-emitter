c97c8ef9ef564de72a499e697c27425ed0eea62c
yoojia <yoojiachen@gmail.com>
2018-12-30 22:47:00 +0800

    EventHandler接口签名增加抛出异常

--- src/main/java/net/nextabc/emitter/EventHandler.java
+++ src/main/java/net/nextabc/emitter/EventHandler.java
@@ -2,12 +2,24 @@ package net.nextabc.emitter;
 
 /**
  * 事件处理器
+ *
  * @author 陈哈哈 (yoojiachen@gmail.com, chenyongjia@parkingwang.com)
  * @version 0.1
  */
 public interface EventHandler {
 
-    void onEvent(Event event);
+    /**
+     * 处理事件
+     *
+     * @param event 事件对象
+     */
+    void onEvent(Event event) throws Exception;
 
-    void onError(Exception error);
+    /**
+     * 处理事件方法函数 {@link EventHandler#onError(Exception)} 发生异常时，可以抛出异常，并由此方法捕获处理。
+     * 如果此方法仍然抛出异常，则由 {@link NextEmitter} 的 UncaughtExceptionHandler 处理。
+     *
+     * @param error 异常对象
+     */
+    void onError(Exception error) throws Exception;
 }
